08cad23bd158cee9d7424137812889c67c21672b
lucible <gobbldygook@letterboxes.org>
2019-11-12 19:25:18 -0600

    input.question for algo moved into separate object

--- exercisesToTypescript/scrabbleScorer.ts
+++ exercisesToTypescript/scrabbleScorer.ts
@@ -6,73 +6,9 @@ interface Algorithm {
     scoreFunction(input: string, pointStruct?: {}): number
 }
 
-// code your transform function here
-function transform(oldObj: {}): {} {
-    let newObj: {} = {}
-    for (let item in oldObj) {
-        for (let i = 0; i < oldObj[item].length; i++) {
-            newObj[`${oldObj[item][i]}`] = item
-        }
-    }
-    return newObj
-}
-
-// code your initialPrompt function here
-function initialPrompt(): number {
-    let options: number[] = [0, 1, 2]
-    let prompt: string = input.question("Welcome to the Scrabble score calculator!\n\nWhich scoring algorithm would you like to use?\n\n0 - Scrabble: The traditional scoring algorithm.\n1 - Simple Score: Each letter is worth 1 point.\n2 - Bonus Vowels: Vowels are worth 3 pts, and consonants are 1pt.\n\nEnter 0, 1, or 2: ")
-    let choice: number = Number(prompt)
-    while (!options.includes(choice)) {
-        prompt = input.question("\nInvalid input. Please enter 0, 1, or 2: ")
-        choice = Number(prompt)
-    }
-    return choice
-}
-
-// code your wordValidate function here
-function wordValidate(word: string): boolean {
-    let test = word.match(/[^a-zA-Z\u0020]/)
-    if (test === null) {
-        return true
-    } else {
-        return false
-    }
-}
-
-function userWord(): string {
-    let entry: string = input.question('\nPlease enter a word to score, or "Stop" to exit the program.\nYou may also type "Algorithm Select" to return to the selection prompt: ')
-    return entry
-}
-
-// code your runProgram function here
-function runProgram(scoreAlgo: Algorithm[]): void {
-    let userChoice = initialPrompt()
-    let input: string = ' '
-
-    console.log(`\nUsing algorithm: ${scoreAlgo[userChoice].name}: ${scoreAlgo[userChoice].description}`)
-
-    input = userWord()
-
-    while (input !== 'Stop') {
-        if (input === 'Stop' || input === 'stop') {
-            break;
-        }
-        
-        if (input.toLowerCase() === 'algorithm select') {
-            userChoice = initialPrompt()
-            console.log(`\nUsing algorithm: ${scoreAlgo[userChoice].name}: ${scoreAlgo[userChoice].description}`)
-            input = userWord()
-        } else {
-            if (wordValidate(input)) {
-                let points = scoreAlgo[userChoice].scoreFunction(input)
-                console.log(`\n>>> Score for "${input}": ${points} <<<`)
-                input = userWord()
-            } else {
-                console.log('Invalid input!')
-                input = userWord()
-            }
-        }
-    }
+interface input {
+    algChoice(): number
+    word(): string
 }
 
 // here is the oldScoreKey object
@@ -84,6 +20,17 @@ const oldScoreKey: {} = {
     5: ['K'],
     8: ['J', 'X'],
     10: ['Q', 'Z']
+}
+
+// code your transform function here
+function transform(oldObj: {}): {} {
+    let newObj: {} = {}
+    for (let item in oldObj) {
+        for (let i = 0; i < oldObj[item].length; i++) {
+            newObj[`${oldObj[item][i]}`] = item
+        }
+    }
+    return newObj
 }
 
 // use the transform function to create your newScoreKey object here
@@ -140,6 +87,76 @@ let vowel: Algorithm = {
 
 // create your scoringAlgorithms array here
 let scoringAlgorithms: Algorithm[] = [scrabble, simple, vowel]
+
+let state: input = {
+    algChoice: function(): number {
+        let options: number[] = [0, 1, 2]
+        let choice: number = Number(input.question("Enter 0, 1, or 2: "))
+        while (!options.includes(choice)) {
+            choice = Number(input.question("\nInvalid input! Please enter 0, 1, or 2: "))
+        }
+        return choice
+    },
+    word: function(): string {
+        let word = input.question('Please enter a word or command: ')
+        let test = word.match(/[^a-zA-Z\u0020]/)
+        return 'ha'
+    }
+}
+
+// code your initialPrompt function here
+function initialPrompt(): number {
+    console.log(`Welcome to the Scrabble score calculator!\n\nWhich scoring algorithm would you like to use?\n`)
+    scoringAlgorithms.map((x:Algorithm, index) => console.log(`${index} - ${x.name}: ${x.description}`))
+    console.log('')
+    return state.algChoice()
+}
+
+// code your wordValidate function here
+function wordValidate(word: string): boolean {
+    let test = word.match(/[^a-zA-Z\u0020]/)
+    if (test === null) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function userWord(): string {
+    let entry: string = input.question('\nPlease enter a word to score, or "Stop" to exit the program.\nYou may also type "Algorithm Select" to return to the selection prompt: ')
+    return entry
+}
+
+// code your runProgram function here
+function runProgram(scoreAlgo: Algorithm[]): void {
+    let userChoice = initialPrompt()
+    let input: string = ' '
+
+    console.log(`\nUsing algorithm: ${scoreAlgo[userChoice].name}: ${scoreAlgo[userChoice].description}`)
+
+    input = userWord()
+
+    while (input !== 'Stop') {
+        if (input === 'Stop' || input === 'stop') {
+            break;
+        }
+        
+        if (input.toLowerCase() === 'algorithm select') {
+            userChoice = initialPrompt()
+            console.log(`\nUsing algorithm: ${scoreAlgo[userChoice].name}: ${scoreAlgo[userChoice].description}`)
+            input = userWord()
+        } else {
+            if (wordValidate(input)) {
+                let points = scoreAlgo[userChoice].scoreFunction(input)
+                console.log(`\n>>> Score for "${input}": ${points} <<<`)
+                input = userWord()
+            } else {
+                console.log('Invalid input!')
+                input = userWord()
+            }
+        }
+    }
+}
 
 // call the runProgram function here
 runProgram(scoringAlgorithms)

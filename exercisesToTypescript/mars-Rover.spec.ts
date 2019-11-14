b9a778e1ddbb1525cf428cf501c53e616b3ff426
lucible <gobbldygook@letterboxes.org>
2019-11-13 20:36:47 -0600

    marsRover: initializing Rover class & spec tests time to head home - I'll add this later! :D

---
+++ exercisesToTypescript/mars-Rover.spec.ts
@@ -0,0 +1,10 @@
+import assert from 'assert'
+import Command from './mars-Command'
+import Message from './mars-Message'
+import Rover from './mars-Rover'
+
+describe("Rover class", () => {
+    it("does something", () => {
+        // put code here
+    })
+})
---
+++ exercisesToTypescript/mars-Rover.ts
@@ -0,0 +1,6 @@
+import Command from './mars-Command'
+import Message from './mars-Message'
+
+export default class Rover {
+    // put code here
+}

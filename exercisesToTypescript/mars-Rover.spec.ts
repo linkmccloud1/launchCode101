db1c133792cbd5b1148449bea6cf9603df421928
lucible <gobbldygook@letterboxes.org>
2019-11-14 16:10:46 -0600

    marsRover: added baseline Rover class & spec tests

--- exercisesToTypescript/mars-Rover.spec.ts
+++ exercisesToTypescript/mars-Rover.spec.ts
@@ -4,7 +4,68 @@ import Message from './mars-Message'
 import Rover from './mars-Rover'
 
 describe("Rover class", () => {
-    it("does something", () => {
-        // put code here
+    it("constructor sets position and default values for mode and generatorWatts", () => {
+        let output = new Rover(100)
+        assert.strictEqual(output.mode, 'NORMAL')
+        assert.strictEqual(output.generatorWatts, 110)
+    })
+
+    it("response returned by receiveMessage contains name of message", () => {
+        let message = new Message('Test message')
+        let rover = new Rover(100)
+        let output = rover.receiveMessage(message)
+        assert.strictEqual(output.message, message.name)
+    })
+
+    it("response returned by receiveMessage includes two results if two commands are sent in the message", () => {
+        let array = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]
+        let message = new Message('Test message', array)
+        let rover = new Rover(100)
+        let output = rover.receiveMessage(message).results
+        assert.strictEqual(output.length, 2)
+    })
+
+    it("responds correctly to status check command", () => {
+        let message = new Message('Test message', [new Command('STATUS_CHECK')])
+        let rover = new Rover(500)
+        let output = rover.receiveMessage(message).results[0]
+
+        assert.notStrictEqual(output.roverStatus, undefined)
+
+        if (output.roverStatus !== undefined) {
+            assert.strictEqual(output.roverStatus.mode, 'NORMAL')
+            assert.strictEqual(output.roverStatus.generatorWatts, 110)
+            assert.strictEqual(output.roverStatus.position, 500)
+        }
+    })
+
+    it("responds correctly to mode change command", () => {
+        let message = new Message('Test message', [new Command('MODE_CHANGE', 'LOW_POWER')])
+        let rover = new Rover(100)
+        let output = rover.receiveMessage(message).results[0]
+        assert.strictEqual(output.completed, true)
+        assert.strictEqual(rover.mode, 'LOW_POWER')
+    })
+
+    it("responds with 'completed: false' when attempting to move in LOW_POWER mode", () => {
+        let message = new Message('Test', [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('MOVE', 20)])
+        let rover = new Rover(100)
+        let output = rover.receiveMessage(message).results[1]
+        assert.strictEqual(output.completed, false)
+    })
+
+    it("responds with position for move command", () => {
+        let message = new Message('Test', [new Command('MOVE', 500)])
+        let rover = new Rover(100)
+        let results = rover.receiveMessage(message)
+        assert.strictEqual(rover.position, 500)
+    })
+
+    it("responds with 'completed: false' and a message when given unknown command", () => {
+        let message = new Message('Test', [new Command('Ahoy, matey!')])
+        let rover = new Rover(100)
+        let output = rover.receiveMessage(message).results[0]
+        assert.strictEqual(output.completed, false)
+        assert.strictEqual(output.message, 'Error: Invalid command!')
     })
 })
--- exercisesToTypescript/mars-Rover.ts
+++ exercisesToTypescript/mars-Rover.ts
@@ -1,6 +1,79 @@
 import Command from './mars-Command'
 import Message from './mars-Message'
 
+type status = {
+    mode: string
+    generatorWatts: number
+    position: number
+}
+
+type report = {
+    completed: boolean
+    message?: string
+    roverStatus?: status
+}
+
+type feed = {
+    message: Message["name"]
+    results: report[]
+}
+
 export default class Rover {
-    // put code here
+    position: number
+    mode: string
+    generatorWatts: number
+
+    constructor(position:number) {
+        this.position = position
+        this.mode = 'NORMAL'
+        this.generatorWatts = 110
+    }
+
+    processMessage(array:Command[]):report[] {
+        let results:report[] = []
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].commandType === 'MOVE') {
+                if (this.mode === 'LOW_POWER') {
+                    results.push({completed: false})
+                } else if (this.mode === 'NORMAL') {
+                    this.position = Number(array[i].value)
+                    results.push({completed: true})
+                }
+            } else if (array[i].commandType === 'STATUS_CHECK') {
+                results.push({
+                    completed: true,
+                    roverStatus: {
+                        mode: this.mode,
+                        generatorWatts: this.generatorWatts,
+                        position: this.position
+                    }
+                })
+            } else if (array[i].commandType === 'MODE_CHANGE') {
+                this.mode = String(array[i].value)
+                results.push({completed: true})
+            } else {
+                results.push({completed: false, message: 'Error: Invalid command!'})
+            }
+        }
+
+        return results
+    }
+
+    receiveMessage(input:Message):feed {
+        let processed:report[]
+
+        if (input.commands !== undefined) {
+            processed = this.processMessage(input.commands)
+        } else {
+            processed = [{completed:false, message: 'No commands given.'}]
+        }
+
+        let output:feed = {
+            message: input.name,
+            results: processed
+        }
+
+        return output
+    }
 }

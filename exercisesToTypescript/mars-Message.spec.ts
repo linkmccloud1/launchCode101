96680040c95e4e07893be4fff0c6bd7eae3afac0
lucible <gobbldygook@letterboxes.org>
2019-11-30 14:29:06 -0600

    marsRover: updating message unit tests

--- exercisesToTypescript/mars-Message.spec.ts
+++ exercisesToTypescript/mars-Message.spec.ts
@@ -1,19 +1,22 @@
 import assert from 'assert'
-import Command from './mars-Command'
-import Message from './mars-Message'
+import c = Command
+import m = Message
 
 describe("Message class", () => {
     // ignoring the first test of the class
     // b/c the compiler catches that error!
 
     it("constructor sets name", () => {
-        let output = new Message('MOVING')
+        let output = m.newMessage('MOVING')
         assert.notStrictEqual(output.name, undefined)
     })
 
     it("contains a commands array passed into the constructor as 2nd argument", () => {
-        let array = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]
-        let output = new Message('Test message', array)
-        assert.notStrictEqual(output.commands, undefined)
+        let array = [c.changeMode('LOW_POWER'), c.checkStatus()]
+        let output = m.newMessage('Test message', array)
+        switch (output.kind) {
+            case 'ECHO' : return assert.fail
+            case 'COMMANDS' : return assert.notStrictEqual(output.commands, undefined)
+        }
     })
 })
